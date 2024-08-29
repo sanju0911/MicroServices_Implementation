441a9908a8e77f5bac427c3b8c9d382d36b5f6d9
sanju <sanjayjanardhan231@gmail.com>
2024-08-29 13:40:21 +0530

    connection to database

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 
+const db = require("./config/db_config");
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.post("/", (req, res) => {
   res.send("hello world");
