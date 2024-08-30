073af57acce1a6d507a6a3ce9d1d065c674483d3
sanju <sanjayjanardhan231@gmail.com>
2024-08-30 12:43:46 +0530

    established the connection betwee the userdetailsa and posts

--- Blog_Making/controllers/Blogcontroller.js
+++ Blog_Making/controllers/Blogcontroller.js
@@ -2,6 +2,40 @@ const Blog = require("../models/Model");
 const axios = require("axios");
 
 class BlogController {
+  static async index(req, res) {
+    try {
+      const blogs = await Blog.find();
+
+      const userIds = blogs.map((blog) => blog.userId);
+
+      const response = await axios.post("http://localhost:3000/getuser", {
+        userIds,
+      });
+
+      const userDetails = response.data;
+
+      const completeUserData = await blogs
+        .map((blog) => {
+          const userDetail = userDetails.find(
+            (user) => user.id === blog.userId
+          );
+          if (userDetail) {
+            return {
+              blog,
+              userDetail,
+            };
+          }
+        })
+        .filter((data) => data !== undefined);
+
+      return res.json({ completeUserData });
+    } catch (error) {
+      console.log("Error fetching blogs:", error);
+      return res
+        .status(500)
+        .json({ message: "Error fetching blogs", error: error.message });
+    }
+  }
   static async store(req, res) {
     try {
       const { userId, content } = req.body;
